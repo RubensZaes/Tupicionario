b4df1e6427dd3ebd4b5003eccb9896c79f838581
RubensZaes <rubenszaes@gmail.com>
2020-04-08 00:18:02 -0300

    Intent creation for activity

--- app/src/main/java/com/example/tupicionario/MainActivity.kt
+++ app/src/main/java/com/example/tupicionario/MainActivity.kt
@@ -1,12 +1,53 @@
 package com.example.tupicionario
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bichosTextView: TextView
+    private lateinit var culinariaTextView: TextView
+    private lateinit var plantasTextView: TextView
+    private lateinit var povosNativosTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bichosTextView = findViewById(R.id.bichosTextView)
+        bichosTextView.setOnClickListener(View.OnClickListener { callActivityBichos() })
+
+        culinariaTextView = findViewById(R.id.culinariaTextView)
+        culinariaTextView.setOnClickListener(View.OnClickListener { callactivityCulinaria() })
+
+        plantasTextView = findViewById(R.id.plantasTextView)
+        plantasTextView.setOnClickListener(View.OnClickListener { callActivityPlantas() })
+
+        povosNativosTextView = findViewById(R.id.povosNativosTextView)
+        povosNativosTextView.setOnClickListener(View.OnClickListener { callActivityPovosNativos() })
+
+    }
+
+    private fun callActivityBichos() {
+        val intent = Intent(this, BichosActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun callactivityCulinaria() {
+        val intent = Intent(this, CulinariaActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun callActivityPlantas() {
+        val intent = Intent(this, PlantasActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun callActivityPovosNativos() {
+        val intent = Intent(this, PovosNativosActivity::class.java)
+        startActivity(intent)
     }
 }
