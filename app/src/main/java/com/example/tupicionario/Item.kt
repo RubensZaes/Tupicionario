deae00c9a2a498626d3c090eed28154ccda694f2
RubensZaes <rubenszaes@gmail.com>
2020-04-20 18:31:33 -0300

    Update Item.kt

--- app/src/main/java/com/example/tupicionario/Item.kt
+++ app/src/main/java/com/example/tupicionario/Item.kt
@@ -1,7 +1,7 @@
 package com.example.tupicionario
 
 data class Item (
-    var titulo: String,
-    var descricao: String,
-    var imagem: Int
+    var titulo: String?,
+    var descricao: String?,
+    var imagem: Int?
 )
